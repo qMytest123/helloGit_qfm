ce63979f1ff9d84709a4a420ad2b5eff251a4aee
Administrator <48076177@qq.com>
2019-04-15 11:22:29 +0800

    first commit all code

---
+++ src/main/java/com/qfmt/hellogit/HellogitApplication.java
@@ -0,0 +1,13 @@
+package com.qfmt.hellogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellogitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellogitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/qfmt/hellogit/hello/HelloController.java
@@ -0,0 +1,17 @@
+package com.qfmt.hellogit.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value="/wel", method = RequestMethod.GET)
+    public String show(Model model){
+
+        model.addAttribute("welcome","hello git");
+        return "result";
+    }
+}
